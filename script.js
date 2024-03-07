2320b82cbbbe712673675405f9acc747568b43ff
jotapegarrafiel <jotapegarrafiel@gmail.com>
2024-03-06 23:26:23 -0300

    Atualizei o texto de meio dia e meia noite

--- script.js
+++ script.js
@@ -7,15 +7,20 @@ function carregar(){
     if(agora >= 6 && agora <= 12){
         img.src = 'dia.png'
         document.body.style.background = '#ffb47c'
+        if (agora == '12'){
+            msg.innerHTML = `Agora é meio dia`
+        }
     } else if (agora >= 13 && agora <= 18) {
         img.src = 'tarde.png'
         document.body.style.background = '#a56965'
-    } else if (agora >=18 && agora <= 0) {
+    } else if (agora >=18) {
         img.src = 'noite.png'
         document.body.style.background = '#1d343a'
     } else { 
         if(agora == '1'){
             msg.innerHTML = `Agora é ${agora} hora.`
+        } if(agora == '0'){
+            msg.innerHTML = `Agora é meia noite`
         }
         img.src = 'madrugada.png'
         document.body.style.background = '#201d24'
